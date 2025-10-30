cf6935a2563b53935d38486c92948d1b4b06e09a
Muhamad Erza Wansyah <erzawansyah@gmail.com>
2025-10-30 08:53:52 +0700

    fix: remove unused test file for SurveyResponseForm component

--- frontend/src/components/forms/SurveyResponseForm.test.tsx
+++
@@ -1,5 +0,0 @@
-// Test file to verify SurveyResponseForm can be imported
-import SurveyResponseForm from './SurveyResponseForm'
-
-// This file should compile if SurveyResponseForm is working
-export const testComponent = SurveyResponseForm
